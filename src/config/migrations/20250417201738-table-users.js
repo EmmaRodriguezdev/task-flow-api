b16c66d568f587a84284e7f0d782071641997165
EmmaRodriguezdev <erodriguezmontes9@gmail.com>
2025-04-17 18:28:06 -0600

    feat: add migrations and seed

---
+++ src/config/migrations/20250417201738-table-users.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
